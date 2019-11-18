b8cc94c1e1b85ea34dc5c0fe316a4d5725daf2c0
James Estevez <jestevez@salesforce.com>
2019-11-18 12:47:00 -0800

    Revert combined task and retry with just preflight check

--- tasks/check_recordtypes.py
+++ tasks/check_recordtypes.py
@@ -1,13 +1,8 @@
 from cumulusci.core.utils import process_list_arg
-from cumulusci.tasks.salesforce import Deploy
-from cumulusci.salesforce_api.utils import get_simple_salesforce_connection
+from cumulusci.tasks.salesforce import BaseSalesforceApiTask
 
 
-class CheckExistingRecordTypes(Deploy):
-    """
-    Checks for existing Opportunity record types, deploys GEM Record Types if not found.
-    """
-
+class CheckExistingRecordTypes(BaseSalesforceApiTask):
     task_options = {
         "sobject": {
             "description": "SObject for which record types will be retrieved",
@@ -16,52 +11,11 @@ class CheckExistingRecordTypes(Deploy):
         "ignored_record_types": {
             "description": "Developer names for record types to ignore."
         },
-        "filtered_record_types": {
-            "description": "Developer names for record types. Blocks deployment if any existing record types with these names is found."
-        },
-        "path": {
-            "description": "The path to the metadata source to be checked",
-            "required": True,
-        },
-        "unmanaged": {
-            "description": "If True, changes namespace_inject to replace tokens with a blank string"
-        },
-        "namespace_inject": {
-            "description": "If set, the namespace tokens in files and filenames are replaced with the namespace's prefix"
-        },
-        "namespace_strip": {
-            "description": "If set, all namespace prefixes for the namespace specified are stripped from files and filenames"
-        },
-        "namespace_tokenize": {
-            "description": "If set, all namespace prefixes for the namespace specified are replaced with tokens for use with namespace_inject"
-        },
-        "static_resource_path": {
-            "description": "The path where decompressed static resources are stored.  Any subdirectories found will be zipped and added to the staticresources directory of the build."
-        },
-        "namespaced_org": {
-            "description": "If True, the tokens %%%NAMESPACED_ORG%%% and ___NAMESPACED_ORG___ will get replaced with the namespace.  The default is false causing those tokens to get stripped and replaced with an empty string.  Set this if deploying to a namespaced scratch org or packaging org."
-        },
     }
 
     def _run_task(self):
-        if self.can_deploy_opp_rts():
-            super()._run_task()
-
-    def can_deploy_opp_rts(self):
-        business_process_exists = self.business_process_exists()
-        record_types_exist = self.record_types_exist()
-        can_deploy = business_process_exists and not record_types_exist
-        self.logger.info(f"{'Deploying...' if can_deploy else 'Skipping...'}")
-        return can_deploy
-
-    def record_types_exist(self):
         sobject = self.options["sobject"]
-        sf = get_simple_salesforce_connection(
-            self.project_config,
-            self.org_config,
-            api_version=self.project_config.api_version,
-        )
-        describe_results = getattr(sf, sobject).describe()
+        describe_results = getattr(self.sf, sobject).describe()
         record_types = [
             rt for rt in describe_results["recordTypeInfos"] if not rt["master"]
         ]
@@ -71,36 +25,12 @@ class CheckExistingRecordTypes(Deploy):
             record_types = [
                 rt for rt in record_types if rt["developerName"] not in ignored_names
             ]
-
-        filtered_names = set(process_list_arg(self.options["filtered_record_types"]))
-        recordtypes_exist = any(
-            rt["developerName"] in filtered_names for rt in record_types
-        )
+        recordtypes_exist = len(record_types) > 0
         self.logger.info(
             f"Existing record types on {sobject} {'not ' if not recordtypes_exist else ''}found."
         )
-        if recordtypes_exist:
-            found_record_types = ", ".join(
-                [
-                    rt["developerName"]
-                    for rt in record_types
-                    if rt["developerName"] in filtered_names
-                ]
-            )
-            self.logger.info(
-                f"Found the following filtered record types: {found_record_types}"
-            )
-        return recordtypes_exist
 
-    def business_process_exists(self):
-        sobject = self.options["sobject"]
-        tooling = get_simple_salesforce_connection(
-            self.project_config,
-            self.org_config,
-            api_version=self.project_config.api_version,
-        )
-        tooling.base_url += "tooling/"
-        business_process_result = tooling.query(
+        business_process_result = self.tooling.query(
             "SELECT Id FROM BusinessProcess WHERE Name = 'NPSP_Default'"
         )
         business_process_exists = business_process_result["size"] > 0
@@ -108,4 +38,4 @@ class CheckExistingRecordTypes(Deploy):
             f"Existing NPSP_Default BusinessProcess on {sobject} {'not ' if not business_process_exists  else ''}found."
         )
 
-        return business_process_exists
+        self.return_values = business_process_exists and not recordtypes_exist
