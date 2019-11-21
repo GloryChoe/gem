5b046c11c072563a3ef2fedaa2020932201c6a5f
James Estevez <jestevez@salesforce.com>
2019-11-20 16:28:49 -0800

    Add record type names to filter list
    We're not using `business_process_exists` in any meaningful way just
    yet, but I'm leaving it in place in case there's some pushback to
    letting deploy_opp_record_types fail noisily when it's missing the NPSP
    business process. I also wonder whether we should refactor that to use
    a GEM business process when `NPSP_Default` is not present.

--- tasks/check_recordtypes.py
+++ tasks/check_recordtypes.py
@@ -1,8 +1,13 @@
 from cumulusci.core.utils import process_list_arg
 from cumulusci.tasks.salesforce import BaseSalesforceApiTask
+from cumulusci.salesforce_api.utils import get_simple_salesforce_connection
 
 
 class CheckExistingRecordTypes(BaseSalesforceApiTask):
+    """
+    Checks for existing Opportunity record types, deploys GEM Record Types if not found.
+    """
+
     task_options = {
         "sobject": {
             "description": "SObject for which record types will be retrieved",
@@ -11,11 +16,28 @@ class CheckExistingRecordTypes(BaseSalesforceApiTask):
         "ignored_record_types": {
             "description": "Developer names for record types to ignore."
         },
+        "filtered_record_types": {
+            "description": "Developer names for record types. Blocks deployment if any existing record types with these names is found.",
+            "required": True,
+        },
     }
 
     def _run_task(self):
+        business_process_exists = self.business_process_exists()
+        record_types_exist = self.record_types_exist()
+        if record_types_exist:
+            self.return_values["record_types_exist"] = True
+        else:
+            self.return_values = None
+
+    def record_types_exist(self):
         sobject = self.options["sobject"]
-        describe_results = getattr(self.sf, sobject).describe()
+        sf = get_simple_salesforce_connection(
+            self.project_config,
+            self.org_config,
+            api_version=self.project_config.api_version,
+        )
+        describe_results = getattr(sf, sobject).describe()
         record_types = [
             rt for rt in describe_results["recordTypeInfos"] if not rt["master"]
         ]
@@ -25,12 +47,36 @@ class CheckExistingRecordTypes(BaseSalesforceApiTask):
             record_types = [
                 rt for rt in record_types if rt["developerName"] not in ignored_names
             ]
-        recordtypes_exist = len(record_types) > 0
+
+        filtered_names = set(process_list_arg(self.options["filtered_record_types"]))
+        recordtypes_exist = any(
+            rt["developerName"] in filtered_names for rt in record_types
+        )
         self.logger.info(
             f"Existing record types on {sobject} {'not ' if not recordtypes_exist else ''}found."
         )
+        if recordtypes_exist:
+            found_record_types = ", ".join(
+                [
+                    rt["developerName"]
+                    for rt in record_types
+                    if rt["developerName"] in filtered_names
+                ]
+            )
+            self.logger.info(
+                f"Found the following filtered record types: {found_record_types}"
+            )
+        return recordtypes_exist
 
-        business_process_result = self.tooling.query(
+    def business_process_exists(self):
+        sobject = self.options["sobject"]
+        tooling = get_simple_salesforce_connection(
+            self.project_config,
+            self.org_config,
+            api_version=self.project_config.api_version,
+        )
+        tooling.base_url += "tooling/"
+        business_process_result = tooling.query(
             "SELECT Id FROM BusinessProcess WHERE Name = 'NPSP_Default'"
         )
         business_process_exists = business_process_result["size"] > 0
@@ -38,4 +84,4 @@ class CheckExistingRecordTypes(BaseSalesforceApiTask):
             f"Existing NPSP_Default BusinessProcess on {sobject} {'not ' if not business_process_exists  else ''}found."
         )
 
-        self.return_values = business_process_exists and not recordtypes_exist
+        return business_process_exists
